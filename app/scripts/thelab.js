35386e8dcf5ecd6b552fbad28afc9f4e3ddddc5b
Alberto Congiu <4lbertoc@gmail.com>
2013-01-22 22:28:21 +0100

    Added some reminder comments to thelab.js

--- app/scripts/thelab.js
+++ app/scripts/thelab.js
@@ -1,4 +1,4 @@
-define(['gameframework/constants', 'gameframework/gamemanager', 'gameframework/pubsub'], function(constants, GameManager, pubSub) {
+define(['gameframework/constants', 'gameframework/gamemanager', 'gameframework/pubsub'], function (constants, GameManager, pubSub) {
     'use strict';
 
     /* Private variables */
@@ -6,47 +6,47 @@ define(['gameframework/constants', 'gameframework/gamemanager', 'gameframework/p
     var _gameScope = null;
 
     /* Private methods */
-    var _addDarknessCallback = function() {
+    var _addDarknessCallback = function () {
         // Runs when the darkness disappears, activating the lab commands.
         var _callbackId;
 
         function _addLabCommands() {
             pubSub.publish('Terminal/write', ['Lights turned on, activating lab.']);
-            _gameScope.addCommand('addSeed', function() {
+            _gameScope.addCommand('addSeed', function () {
                 // TODO
             });
-            _gameScope.addCommand('getMotherDna', function() {
+            _gameScope.addCommand('getMotherDna', function () {
                 // TODO
             });
-            _gameScope.addCommand('getBugDna', function() {
+            _gameScope.addCommand('getBugDna', function () {
                 // TODO
             });
-            _gameScope.addCommand('setMotherDna', function(dna) {
+            _gameScope.addCommand('setMotherDna', function (dna) {
                 // TODO
             });
-            _gameScope.addCommand('setBugDna', function(dna) {
+            _gameScope.addCommand('setBugDna', function (dna) {
                 // TODO
             });
 
-            if (_callbackId) {
+            if(_callbackId) {
                 pubSub.publish('MutationObserver/remove', [_callbackId]);
             }
         }
         pubSub.publish('MutationObserver/add', [constants.JQ_DARKNESS.parent(), _addLabCommands, [{
             'element': '#darkness'
-        }], function(id) {
+        }], function (id) {
             _callbackId = id;
         }]);
     };
 
-    var _initVars = function() {
+    var _initVars = function () {
         _gameManager = new GameManager();
         _gameManager.init();
         _gameScope = _gameManager.getTerminalScope();
     };
 
     var TheLabGame = {
-        start: function() {
+        start: function () {
             _initVars();
             // TODO
             // Create dish and flask here or create them in the html
