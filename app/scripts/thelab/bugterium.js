6f7933bc9caa02558c5eefedc28fdc40dcbf548f
Alberto Congiu <4lbertoc@gmail.com>
2013-01-29 22:28:21 +0100

    Add creation of bugteria

--- app/scripts/thelab/bugterium.js
+++ app/scripts/thelab/bugterium.js
@@ -1,7 +1,9 @@
 define(['gameframework/pubsub'], function (pubSub) {
+  'use strict';
 
   /* Private variables */
   var _baseDna = {
+    'aspect': 'http://bdogames.altervista.org/gfc-jsbugslab-static/1359505963_bacteria.png',
     'onCreate': {
 
     },
@@ -10,17 +12,30 @@ define(['gameframework/pubsub'], function (pubSub) {
     },
     'replicationSpeed': 10000
   };
+  var _idCounter = 0;
 
   /* PubSub */
-  pubSub.subscribe('Bugterium/hatch', function(refDomNode, dna) {
+  pubSub.subscribe('Bugterium/hatch', function (refDomNode, dna) {
     // Add bugteria to dish
   });
 
-  var Bugterium = function () {};
-  Bugterium.prototype = {
-    getBaseDna: function () {
-      return _baseDna;
+  var Bugterium = function (refDomNode, dna) {
+    this.id = _idCounter++;
+    this.dna = dna;
+
+    var bugDomNode = document.createElement('div');
+    bugDomNode.classList.add('bug');
+    bugDomNode.style.background = 'url(' + dna['aspect'] + ') center no-repeat';
+    if(refDomNode instanceof HTMLElement) {
+      refDomNode.appendChild(bugDomNode);
     }
   };
+  Bugterium.prototype = {};
+
+  /* Static */
+  Bugterium.getBaseDna = function () {
+    return _baseDna;
+  };
+
   return Bugterium;
 });
--- app/scripts/thelab/spore.js
+++ app/scripts/thelab/spore.js
@@ -1,29 +1,86 @@
-define(['gameframework/pubsub'], function (pubSub) {
+define(['./bugterium', 'gameframework/pubsub'], function (Bugterium, pubSub) {
   'use strict';
 
   /* Private variables */
   var _hatchTimeout = 3000;
+  var _minMaxBugteriaCreated = [1, 4];
+  var _baseDna = Bugterium.getBaseDna();
+  var _baseDnaImageSize = null;
+  var _preloadedImages = {};
 
-  var Spore = function (refNode, dna) {
-    this.refNode = refNode;
-    this.dna = dna;
+  /* Private methods */
+  var _getNumberOfCreatedBugteria = function () {
+    return Math.ceil((_minMaxBugteriaCreated[0] - 1) + (Math.random() * (_minMaxBugteriaCreated[1] - (_minMaxBugteriaCreated[0] - 1))));
+  };
 
-    this._hatch(dna);
+  var _preloadImage = function (imageUrl, callback) {
+    var preloadedImage = _preloadedImages[imageUrl];
+    if(preloadedImage instanceof Array) {
+      callback('ok', preloadedImage);
+    } else if(typeof imageUrl === 'string') {
+      var imgDomNode = document.createElement('img');
+      imgDomNode.style.display = 'none';
+      imgDomNode.onload = function () {
+        var dimensions = [imgDomNode.naturalWidth, imgDomNode.naturalHeight];
+        _preloadedImages[imageUrl] = dimensions;
+        var refDomNode = imgDomNode.parentElement;
+        if(refDomNode instanceof HTMLElement) {
+          refDomNode.removeChild(imgDomNode);
+        }
+        if(typeof callback === 'function') {
+          callback('ok', dimensions);
+        }
+      };
+      imgDomNode.onerror = function () {
+        var refDomNode = imgDomNode.parentElement;
+        if(refDomNode instanceof HTMLElement) {
+          refDomNode.removeChild(imgDomNode);
+        }
+        if(typeof callback === 'function') {
+          callback('error');
+        }
+      };
+      imgDomNode.src = imageUrl;
+      document.body.appendChild(imgDomNode);
+    }
+  };
+
+  /* Initialization */
+  _preloadImage(_baseDna['aspect'], function(status, dimensions) {
+      _baseDnaImageSize = (status === 'ok') ? dimensions : [64, 64];
+  });
+
+  var Spore = function (refDomNode, dna) {
+    this.refDomNode = refDomNode;
+    this.dna = dna || _baseDna;
+
+    this._hatch(this.dna);
   };
   Spore.prototype = {
     _hatch: function (dna) {
-      var refDomNode = document.getElementById(this.refNode);
+      var refDomNode = document.getElementById(this.refDomNode);
       if(refDomNode instanceof HTMLElement) {
         var sporeNode = document.createElement('div');
         sporeNode.classList.add('spore');
         refDomNode.appendChild(sporeNode);
 
-        setTimeout(function () {
-          pubSub.publish('Bugterium/hatch', [refDomNode, dna]);
-          if(sporeNode.parentElement === refDomNode) {
-            refDomNode.removeChild(sporeNode);
+        _preloadImage(dna['aspect'], function (status, dimensions) {
+          if(status === 'error') {
+            dna['aspect'] = _baseDna['aspect'];
+            dimensions = _baseDnaImageSize;
           }
-        }, _hatchTimeout);
+          setTimeout(function () {
+            pubSub.publish('Bugterium/hatch', []);
+            if(sporeNode.parentElement === refDomNode) {
+              refDomNode.removeChild(sporeNode);
+            }
+            var numBugteria = _getNumberOfCreatedBugteria();
+            while(numBugteria > 0) {
+              new Bugterium(refDomNode, dna);
+              --numBugteria;
+            }
+          }, _hatchTimeout);
+        });
       }
     }
   };
