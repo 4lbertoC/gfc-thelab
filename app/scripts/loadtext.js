4d14deae548e7a1beb6cc4a7d2a7c53303f3820f
Alberto Congiu <4lbertoc@gmail.com>
2013-02-02 19:28:51 +0100

    use timeout instead of interval in loadtext

--- app/scripts/loadtext.js
+++ app/scripts/loadtext.js
@@ -1,1 +1,32 @@
-(function(){var a=document.getElementById("loadText"),b=0,c=["Almost done...","One more second...","Another script loaded...","Images fetched...","Something else is ready...","Keep reading...","Only few files left to finish...","NULL POINTER EXCEPTION","Just joking...","Your connection is SLOW.","Let's start again...","Loading..."],d=c.length,e=function(){return document.all?a.innerText:a.textContent},f=function(b){document.all?a.innerText=b:a.textContent=b},g=setInterval(function(){if(a&&a.parentElement&&"Done!"!=e()){var h=b++%d;f(c[h])}else clearInterval(g)},500)})();
+(function () {
+    var t = document.getElementById('loadText');
+    var i = 0;
+    var txtOptions = ['Almost done...', 'One more second...', 'Another script loaded...', 'Images fetched...', 'Something else is ready...',
+                            'Keep reading...', 'Only few files left to finish...', 'NULL POINTER EXCEPTION', 'Just joking...', 'Your connection is slow, you know?',
+                            'Let\'s start again...', 'Loading...'];
+    var txtOptionsLength = txtOptions.length;
+    var getText = function () {
+        if(document.all) {
+            return t.innerText;
+        } else {
+            return t.textContent;
+        }
+    }
+    var setText = function (txt) {
+        if(document.all) {
+            t.innerText = txt;
+        } else {
+            t.textContent = txt;
+        }
+    }
+    var changeTextAgain = function() {
+        setTimeout(function () {
+            if(t && t.parentElement && getText() != 'Done!') {
+                var idx = i++ % txtOptionsLength;
+                setText(txtOptions[idx]);
+                changeTextAgain();
+            }
+        }, 500);
+    };
+    changeTextAgain();
+})();
